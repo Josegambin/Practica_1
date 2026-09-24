d3d9d6d26a0dba04504983ad5bf9c3783bf16d5e
Jose Gambin <josemanuel.gambin@gmail.com>
2026-09-24 10:02:08 +0200

    practica 1 php

---
+++ ws/models/User.php
@@ -0,0 +1,94 @@
+<?php
+
+require_once __DIR__ . '/interfaces/IToJson.php';
+
+class User implements IToJson
+{
+    public $nombre;
+    public $apellidos;
+    public $telefono;
+    public $email;
+    public $sexo;
+    public $acciones;
+
+    public function __construct($nombre, $apellidos, $telefono, $email, $sexo, $acciones)
+    {
+        $this->nombre = $nombre;
+        $this->apellidos = $apellidos;
+        $this->telefono = $telefono;
+        $this->email = $email;
+        $this->sexo = $sexo;
+        $this->acciones = $acciones;
+    }
+
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+        return $this;
+    }
+
+    public function getApellidos()
+    {
+        return $this->apellidos;
+    }
+
+    public function setApellidos($apellidos)
+    {
+        $this->apellidos = $apellidos;
+        return $this;
+    }
+
+    public function getTelefono()
+    {
+        return $this->telefono;
+    }
+
+    public function setTelefono($telefono)
+    {
+        $this->telefono = $telefono;
+        return $this;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function setEmail($email)
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+    public function getSexo()
+    {
+        return $this->sexo;
+    }
+
+    public function setSexo($sexo)
+    {
+        $this->sexo = $sexo;
+        return $this;
+    }
+
+    public function getAcciones()
+    {
+        return $this->acciones;
+    }
+
+    public function setAcciones($acciones)
+    {
+        $this->acciones = $acciones;
+        return $this;
+    }
+
+    public function toJson(): string
+    {
+        return json_encode($this) . PHP_EOL;
+    }
+}
---
+++ ws/models/interfaces/crearUsuario.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once __DIR__ . '/../User.php';
+
+header('Content-Type: application/json; charset=utf-8');
+
+$nombre  = $_POST['nombre']  ?? '';
+$apellidos  = $_POST['apellidos']  ?? '';
+$password  = $_POST['password']  ?? '';
+$email  = $_POST['email']  ?? '';
+$telefono  = $_POST['telefono']  ?? '';
+$sexo = $_POST['sexo'] ?? ''; 
+$acciones = $_POST['acciones']  ?? '';
+
+$user = new User($nombre, $apellidos, $telefono, $email, $sexo, $acciones);
+
+$rutaFichero = __DIR__ . '/usuarios.txt';
+$linea = $user->toJson();
+
+if (file_put_contents($rutaFichero, $linea, FILE_APPEND | LOCK_EX) === false) {
+    http_response_code(500);
+    echo json_encode(['error' => 'No se pudo guardar en el fichero']);
+    exit;
+}
+
+echo $user->toJson();
+
+
